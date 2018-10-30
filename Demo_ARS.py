97d52cc1f313eb5d0d04d8e10827fe893e76546e
Prabhakar <prk327@gmail.com>
2018-10-30 11:03:18 +0530

    Create Demo_ARS.py

---
+++ Demo_ARS.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Running the main code
+data = pd.read_csv("PG_2007_2017.csv")
+hp = Hp()
+np.random.seed(hp.seed)
+
+env = AlgoTradingEnv(data,3,1)
+nb_inputs = env.observation_space[0].shape[0]
+nb_outputs = env.action_space[0].shape[0]
+policy = Policy(nb_inputs, nb_outputs)
+normalizer = Normalizer(nb_inputs)
+train(env, policy, normalizer, hp)
