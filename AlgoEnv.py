0756e4492f748cbe949e4d8c5d58bdf9ee6d1ffd
Prabhakar <prk327@gmail.com>
2018-10-30 10:55:19 +0530

    Update AlgoEnv.py

--- AlgoEnv.py
+++ AlgoEnv.py
@@ -1,13 +1,15 @@
 class AlgoTradingEnv():
-	"""docstring for AlgoTrading"""
-	def __init__(self, DataFrame):
+	"""This will create a algotrading env which accept data frame for training AI models"""
+	def __init__(self, DataFrame, XColFromLeft, YcolFromRight):
 		self.i = 0
+		self.x = XColFromLeft
+		self.y = YcolFromRight
 		self.observation_space = []
 		self.action_space = []
 		self.DataFrame = DataFrame
 		self.numOfEpi = len(self.DataFrame.index)
-		self.observation_space.append(self.DataFrame.loc[self.i][0])
-		self.action_space.append(self.DataFrame.loc[self.i][1])
+		self.observation_space.append(self.DataFrame.loc[self.i][:self.x].values)
+		self.action_space.append(self.DataFrame.loc[self.i][-self.y:].values)
 
 	def step(self, action):
 		self.action = action
@@ -15,28 +17,24 @@ class AlgoTradingEnv():
 		reward = 0.0
 		state = 0.0
 		if self.i == self.numOfEpi:
-			done = True
-			print (" State: ", self.observation_space, '\n', "Action: ", self.action_space)
+			done = True	
 		else:
 			done = False
 			reward = self.RLReward()
-			state = self.DataFrame.loc[self.i][0]
-			self.observation_space[0] = self.DataFrame.loc[self.i][0]
-			self.action_space[0] = self.DataFrame.loc[self.i][1]
+			state = self.DataFrame.loc[self.i][:self.x].values            
+			self.observation_space[0] = self.DataFrame.loc[self.i][:self.x].values
+			self.action_space[0] = self.DataFrame.loc[self.i][-self.y:].values
 		return (state, reward, done)
 
 	def reset(self):
 		self.i = 0
-		self.observation_space[0] = self.DataFrame.loc[self.i][0]
-		self.action_space[0] = self.DataFrame.loc[self.i][1]
-		return self.observation_space
-
+		self.observation_space[0] = self.DataFrame.loc[self.i][:self.x].values
+		self.action_space[0] = self.DataFrame.loc[self.i][-self.y:].values
+		return self.observation_space[0]
+    
 	def RLReward(self):
-	    dif = 0
-	    for O, A in zip(self.action_space, self.action):
-	        if O - A == 0:
-	            dif += 1
-	        else:
-	            dif += 0
-	    reward = dif/len(self.action_space)
-	    return reward
+		dif = 0
+		reward = 0
+		for O, A in zip(self.action_space[0], self.action):
+			reward += (A - O) / O
+		return reward
