2454f8e0664cc2d9345b3080c17db617619bdbe3
Prabhakar <prk327@gmail.com>
2018-11-08 10:47:34 +0530

    Update AlgoEnv.py

--- AlgoEnv.py
+++ AlgoEnv.py
@@ -1,5 +1,5 @@
 class AlgoTradingEnv():
-	"""This will create a algotrading env which accept data frame for training AI models"""
+	"""docstring for AlgoTrading"""
 	def __init__(self, DataFrame, XColFromLeft, YcolFromRight):
 		self.i = 0
 		self.x = XColFromLeft
@@ -17,24 +17,50 @@ class AlgoTradingEnv():
 		reward = 0.0
 		state = 0.0
 		if self.i == self.numOfEpi:
-			done = True	
+			done = True
 		else:
 			done = False
 			reward = self.RLReward()
-			state = self.DataFrame.loc[self.i][:self.x].values            
+			state = self.DataFrame.loc[self.i][:self.x].values          
 			self.observation_space[0] = self.DataFrame.loc[self.i][:self.x].values
 			self.action_space[0] = self.DataFrame.loc[self.i][-self.y:].values
 		return (state, reward, done)
 
 	def reset(self):
+		self.Actual = []
+		self.Predicted = []
+		self.Date = []
+		self.R = 0.
+		self.AbsuluteDifference = 0.
+		self.SquareDifference = 0.
 		self.i = 0
+		self.df = {}
 		self.observation_space[0] = self.DataFrame.loc[self.i][:self.x].values
 		self.action_space[0] = self.DataFrame.loc[self.i][-self.y:].values
 		return self.observation_space[0]
-    
+
 	def RLReward(self):
-		dif = 0
+		self.AD = 0.
+		self.SD = 0.
 		reward = 0
-		for O, A in zip(self.action_space[0], self.action):
+		MAEReward = 0.
+		for O, A, Date in zip(self.action_space[0], self.action, self.observation_space[0]): 
 			reward += (A - O) / O
-		return reward
+			self.AD += abs(O - A)
+			self.SD += (O - A) ** 2
+		self.R += reward
+		self.AbsuluteDifference += self.AD
+		self.SquareDifference += self.SD
+		self.Actual.append(O)
+		self.Predicted.append(A)
+		self.Date.append(Date)
+		return self.R
+    
+	def Summary(self):
+		self.MAE = self.AbsuluteDifference / self.i
+		self.MSE = self.SquareDifference / self.i
+		self.RMSE = self.MSE ** (1/2)
+		self.df["Date"] = self.Date
+		self.df["Actual"] = self.Actual
+		self.df["Predicted"] = self.Predicted
+		return None
